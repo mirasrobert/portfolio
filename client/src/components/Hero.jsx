de0d58d5497e760dae4b21fa85d6c5aaa8c5dccc
mirasrobert <mrmirasrobert@gmail.com>
2022-03-07 13:11:24 +0800

    Change Hero Caption

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -9,7 +9,8 @@ const Hero = () => {
       <div className='hero-wrapper mt-5 md:mt-8 flex justify-center items-center'>
         <div className='flex flex-col justify-center items-center'>
           <h5 className='hero-caption font-bold text-center'>
-            Hi, I'm Aspiring To Be a Web Developer
+            Hi, I am a Web Developer
+            {/* Hi, I'm Aspiring To Be a Web Developer */}
           </h5>
           <p className='hero-subtitle-size text-gray-800 text-center'>
             My passion lies in solving real world problems with programming.
